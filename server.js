41db7ceb7d3ab6834acc5eb1ce52b226a03b68ce
Albert Reel <waxhoya@gmail.com>
2016-10-26 16:35:00 -0700

    Per David G. I should actually call the object BEFORE using it... who would have thunk it.

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const ChatRoom = require('./chatRoom');
 
 
 const server = net.createServer(client => {
-
+    const chatRoom = new ChatRoom();
     client.setEncoding('utf-8');
-    ChatRoom.add('client');
+    chatRoom.add('client');
     client.on('data', message => {
         chatRoom.chat(client, message);
     });
     client.on('close', () => {
-        ChatRoom.leaveChatRoom(client);
+        chatRoom.leaveChatRoom(client);
     });
 });
 
