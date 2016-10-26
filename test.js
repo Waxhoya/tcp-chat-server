936b7b9ed3d579135f956b6b21d5737915adc0e4
Albert Reel <waxhoya@gmail.com>
2016-10-26 16:05:48 -0700

    first test passes: CREATE THE SERVER bug fixed

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert
 const fs = require('fs');
-const chatRoom = require('./chatRoom');
+const ChatRoom = require('./chatRoom');
 
 class Tester {
     write(text) {
@@ -8,17 +8,20 @@ class Tester {
     }
 };
 
+const chatRoom = new ChatRoom();
 const testUser1 = new Tester;
 const testUser2 = new Tester;
 
 describe ('Chat function check', () => {
     it('Chat client connects to server', () => {
         chatRoom.add(testUser1);
-        assert.equal('client 1', chatRoom.clients[0].name, "client 1 matches client[1]");
+        console.log('user 1 added');
+        console.log(chatRoom.clients[0].name);
+        assert.equal('User 1', chatRoom.clients[0].name, "client 1 matches client[1]");
         chatRoom.add(testUser2);
-        let connectedUsers = ChatRoom.clients.map(function(liveClient){
+        let connectedUsers = chatRoom.clients.map(function(liveClient){
             return liveClient.name;
         });
-        assert.sameMembers(['Client 1','Client 2'], clientNames, 'Confirmed array match');
+        assert.sameMembers(['User 1','User 2'], connectedUsers, 'Confirmed array match');
     });
 });
