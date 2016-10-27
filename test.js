d8036335e62c05c4f5b94073ec1e576a3558a3e7
Albert Reel <waxhoya@gmail.com>
2016-10-26 17:13:30 -0700

    test 2 complete

--- test.js
+++ test.js
@@ -15,8 +15,6 @@ const testUser2 = new Tester;
 describe ('Chat function check', () => {
     it('Chat client connects to server', () => {
         chatRoom.add(testUser1);
-        console.log('user 1 added');
-        console.log(chatRoom.clients[0].name);
         assert.equal('User 1', chatRoom.clients[0].name, "client 1 matches client[1]");
         chatRoom.add(testUser2);
         let connectedUsers = chatRoom.clients.map(function(liveClient){
@@ -24,4 +22,15 @@ describe ('Chat function check', () => {
         });
         assert.sameMembers(['User 1','User 2'], connectedUsers, 'Confirmed array match');
     });
+
+    it('Checks that messages are sent', () => {
+
+        chatRoom.chat(testUser1, 'Im a test!');
+        console.log(testUser2.recieved);
+        console.log("user one: ", testUser1.recieved)
+        assert.equal(testUser2.recieved, 'User 1: Im a test!');
+        assert.equal(testUser1.recieved, 'User 2: entered the room');   
+    });
+
+
 });
